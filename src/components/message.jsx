13bd4be0291cbeb593aee84432e0999f316694b0
Kavya Aella <kaella5622@gmail.com>
2020-05-05 17:02:12 -0400

    Initial Commit

---
+++ src/components/message.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+class Message extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          visible: 0
+        };
+     }
+    
+   clickHandler = e =>
+    {
+        e.preventDefault();
+        this.setState({
+          visible: !this.state.visible
+        });
+    };
+    
+    render() {
+        return (
+            <React.Fragment>
+              <a href="#" onClick={this.clickHandler}>
+                Want to buy a new car?
+              </a>
+              {this.state.visible && <p>Call +11 22 33 44 now!</p>}
+            </React.Fragment>
+          );
+        }
+}
+ 
+export default Message;
